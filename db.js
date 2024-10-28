3492a0eadcd4facecd68e301bb0af7d2a0bd5ce7
rhema-proconnect <proconnect.ca2024@gmail.com>
2024-10-28 12:12:59 +0000

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ module.exports = () => {
 		useUnifiedTopology: true,
 	};
 	try {
-		mongoose.connect(process.env.DB, connectionParams);
+		mongoose.connect("mongodb+srv://EyichemDotCom:Eyichem.com@cluster0.ihpll.mongodb.net/Uh?retryWrites=true&w=majority", connectionParams);
 		console.log("Connected to database successfully");
 	} catch (error) {
 		console.log(error);
